6a7cbca1bc3be709f5d2d201a7d8ba198c61c915
Peppernrino <Peppernrino@users.noreply.github.com>
2017-04-09 12:06:44 -0600

    Update GridcoinAutoNode.sh
    Changed gridcoin-daily to gridcoin-stable.
    Removed unnecessary sudo.
    Simplified packages section.
    Updated addnode list.
    Updated package removal section.
    Removed dialog installation.

--- GridcoinAutoNode.sh
+++ GridcoinAutoNode.sh
@@ -1,51 +1,38 @@
 #!/bin/bash
 echo "########### The server will reboot when the script is complete"
-echo "########### Changing to home dir"
+echo "########### Changing to /home/ dir"
 cd ~
- 
-echo "########### Adding Google DNS"
-echo "dns-nameservers 8.8.8.8 8.8.4.4" >> /etc/network/interface
- 
-echo "########### Installing necessary packages"
-apt-get -y dist-upgrade
-apt-get -y update && apt-get -y upgrade; apt-get install -y --no-install-recommends apt-utils && apt-get -y install dialog && apt-get -y install software-properties-common && apt-get -y install sudo && apt-get -y install git && apt-get -y install gcc && apt-get -y install make && apt-get -y install unzip
-add-apt-repository -y ppa:gridcoin/gridcoin-daily
-apt-get -y update && apt-get -y upgrade; apt-get -y install gridcoinresearchd
- 
+
 # echo "########### Disabling IPv6"
 # sed -i '$anet.ipv6.conf.all.disable_ipv6 = 1' /etc/sysctl.conf
 # sed -i '$anet.ipv6.conf.default.disable_ipv6 = 1' /etc/sysctl.conf
 # sed -i '$anet.ipv6.conf.lo.disable_ipv6 = 1' /etc/sysctl.conf
-# sudo sysctl -p
- 
+# sysctl -p
+
+echo "########### Adding Google DNS"
+echo "dns-nameservers 8.8.8.8 8.8.4.4" >> /etc/network/interfaces
+
+echo "########### Adding Spideroak source"
+echo "38.121.104.79 spideroak.com" >> /etc/hosts
+
+echo "########### Installing necessary packages"
+apt-get -y dist-upgrade
+apt-get -y update && apt-get -y upgrade; apt-get install -y --no-install-recommends apt-utils && apt-get -y install software-properties-common && apt-get -y install sudo && apt-get -y install git && apt-get -y install gcc && apt-get -y install make && apt-get -y install unzip
+add-apt-repository -y ppa:gridcoin/gridcoin-stable
+apt-get -y update && apt-get -y upgrade; apt-get -y install gridcoinresearchd
+
 echo "########### Creating gridcoin user"
 useradd -m gridcoin
- 
+
 echo "########### Creating gridcoinresearch.conf file"
 cd ~gridcoin
 sudo -u gridcoin mkdir .GridcoinResearch
 cd /home/gridcoin/.GridcoinResearch/
- 
-echo "########### Downloading blockchain files..."
-# Uncomment sources for faster trusted member downloads over SSL
-# Gridcoin.us: Official source
-# Google Drive (bit.ly): managed by NeuralMiner
-# SpiderOak: managed by Peppernrino
- 
-# sudo -u gridcoin wget http://download.gridcoin.us/download/downloadstake/signed/snapshot.zip
-# sudo -u gridcoin wget https://bit.ly/GRCSnapshot
-# sudo -u gridcoin wget https://spideroak.com/share/N4YFAZLQOBSXEMDP/public/d%3A/Gridcoin.Tools/Share/snapshot.zip
-# sudo unzip snapshot.zip
- 
-# Use bootstrap of blockchain vs. snapshot. Slower, but more verification. (Recommended method)
-# sudo -u gridcoin wget https://bit.ly/GRCBootstrap
+
+echo "########### Downloading and extracting bootstrap.zip"
 sudo -u gridcoin wget https://spideroak.com/share/N4YFAZLQOBSXEMDP/public/d%3A/Gridcoin.Tools/Share/bootstrap.zip
 sudo unzip bootstrap.zip
- 
-echo "########### Deleting .zip package to save space :)"
-# sudo rm ./snapshot.zip
-sudo rm ./bootstrap.zip
- 
+
 sudo chown -R gridcoin:gridcoin /home/gridcoin/.GridcoinResearch/*
 config="gridcoinresearch.conf"
 sudo -u gridcoin touch $config
@@ -59,13 +46,18 @@ echo "enablespeech=false" >> $config
 echo "poolmining=false" >> $config
 echo "UpdatingLeaderboard=false" >> $config
 echo "cpumining=false" >> $config
+echo "addnode=45.32.175.233" >> $config
+echo "addnode=45.76.142.167" >> $config
+echo "addnode=45.76.173.179" >> $config
+echo "addnode=45.76.182.98" >> $config
 echo "addnode=104.244.75.96" >> $config
-echo "addnode=amsterdam.grcnode.co.uk" >> $config
-echo "addnode=frankfurt.grcnode.co.uk" >> $config
+echo "addnode=139.162.171.24" >> $config
 echo "addnode=grc.z9.de" >> $config
 echo "addnode=grcnode01.centralus.cloudapp.azure.com" >> $config
 echo "addnode=grcnode02.eastus.cloudapp.azure.com" >> $config
-echo "addnode=grcnode03.westus.cloudapp.azure.com" >> $config
+echo "addnode=grcnode03.eastus.cloudapp.azure.com" >> $config
+echo "addnode=grcnode04.eastus.cloudapp.azure.com" >> $config
+echo "addnode=grcnode05.westus2.cloudapp.azure.com" >> $config
 echo "addnode=gridcoin.asia" >> $config
 echo "addnode=gridcoin.is.dopeshit.net" >> $config
 echo "addnode=ils.gridcoin.co.il" >> $config
@@ -80,16 +72,16 @@ randUser=`< /dev/random tr -dc A-Za-z0-9 | head -c64`
 randPass=`< /dev/random tr -dc A-Za-z0-9 | head -c64`
 echo "rpcuser=$randUser" >> $config
 echo "rpcpassword=$randPass" >> $config
- 
+
 echo "########### Setting up chrony"
 cd ~
 git clone git://git.tuxfamily.org/gitroot/chrony/chrony.git
 cd ~/chrony
 ./configure
- 
+
 echo "########### Cleaning up unnecessary packages"
-apt-get -y remove git && apt-get -y remove gcc && apt-get -y remove unzip
+apt-get -y remove git && apt-get -y remove gcc && apt-get -y remove make && apt-get -y remove unzip
 apt-get -y autoremove
- 
+
 echo "########### All done! Thank you for supporting the Gridcoin network! Server will now start."
 su - gridcoin -c gridcoinresearchd
